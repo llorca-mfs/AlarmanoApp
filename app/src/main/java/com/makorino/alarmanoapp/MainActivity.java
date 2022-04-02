fc35bb184f17c5c90530af6a083c609f22cbc293
Marc Llorca <llorca.mfs@gmail.com>
2022-04-02 15:57:09 +0800

    Added debug buttons with basic functionality

--- app/src/main/java/com/makorino/alarmanoapp/MainActivity.java
+++ app/src/main/java/com/makorino/alarmanoapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.makorino.alarmanoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.makorino.alarmanoapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        binding.btnMagnanakaw.setOnClickListener(v ->{
+            Toast.makeText(this, "Magnanakaw detected!", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.btnNotmagnanakaw.setOnClickListener(v ->{
+            Toast.makeText(this, "Hindi Magnanakaw", Toast.LENGTH_SHORT).show();
+        });
+
+        setContentView(binding.getRoot());
     }
 }
